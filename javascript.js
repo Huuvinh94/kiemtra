7e22f0149023f7c74e06f7af3fb1cd9f1754e08e
HuuVinh94 <tnhuuvinh31031994@gmail.com>
2016-12-26 09:07:04 +0700

    moi

---
+++ javascript.js
@@ -0,0 +1,15 @@
+//Cau 1
+$( document ).ready(function(){
+	$("#changeColorDiv").on("click", function() {
+    	$(this).css("background", "red");
+});
+
+	//var t = $('*:contains("Paragraph")');
+
+		$("h1").click(function() {
+		$("p").append("class = “myclass”");
+	});
+		
+
+});
+
